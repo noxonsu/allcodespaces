a7ceb8191ca45f513921db4f1976835e2ce3bf20
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2025-07-17 22:12:22 +0300

    Version 1.3.6
    fix static

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -72,11 +72,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
