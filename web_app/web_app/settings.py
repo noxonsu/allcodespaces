c0090feb2f1e6bcfa6deee684ab260ed4ffdabb3
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2025-07-27 22:39:56 +0300

    Version 1.5.3
    Add fixes

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -189,7 +189,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/core/campaignchannel/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
