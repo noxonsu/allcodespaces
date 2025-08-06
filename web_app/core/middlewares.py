d8bf2419ad0f2937b794fe92676878d1ea79b7f4
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2025-08-06 11:33:18 +0300

    Version 1.5.13
    Add fixes

--- web_app/core/middlewares.py
+++ web_app/core/middlewares.py
@@ -2,6 +2,7 @@
 from typing import Protocol
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect
+from django.utils.deprecation import MiddlewareMixin
 from rest_framework.exceptions import PermissionDenied
 from django.utils import timezone
 from web_app.logger import logger
@@ -23,6 +24,7 @@ class MiddlewareProtocol(Protocol):
 
         return response
 
+# class IPMiddleware(MiddlewareMixin):
 class IPMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -34,7 +36,7 @@ class IPMiddleware:
         print(f'IPMiddleware {ip=} init connection.')
         if ip in set(settings.IP_BLOCKLIST):
             logger.info(f'IPMiddleware blocked {ip=} tried to access app at {timezone.now()}')
-            redirect_to = request.headers.get('referer', request.META.get('HTTP_REFERER', 'https://example.com'))
+            redirect_to = 'https://example.com'
             return HttpResponsePermanentRedirect(redirect_to=redirect_to)
         response = self.get_response(request)
 
